56ac45388fcde10d21f8cb712ed9086c6ce4fe31
Gabriele99-developer <79506019+Gabriele99-developer@users.noreply.github.com>
2021-05-01 21:27:33 +0200

    sistemato "button" sistemato background del contatore sistemato bordi contatore

--- js/funzioni.js
+++ js/funzioni.js
@@ -1,10 +1,10 @@
-let add = document.getElementById('decrementatore');
-let remove = document.getElementById('incrementatore');
+let remove = document.getElementById('decrementatore');
+let add = document.getElementById('incrementatore');
 
 let int = document.getElementById('numero');
 let integer = 0;
 
-add.addEventListener('click', function(){
+remove.addEventListener('click', function(){
   integer -=1;
   int.innerHTML = integer;
 })
