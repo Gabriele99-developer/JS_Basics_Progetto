6a77aa5b3e292bd275af37cff19ba4551638a43e
Gabriele99-developer <79506019+Gabriele99-developer@users.noreply.github.com>
2021-05-01 15:01:12 +0200

    scritto codice html,css e js per contatore

--- js/funzioni.js
+++ js/funzioni.js
@@ -1,3 +1,15 @@
-function aumentaNumero(n){
-    let n;
-}
+let add = document.getElementById('decrementatore');
+let remove = document.getElementById('incrementatore');
+
+let int = document.getElementById('numero');
+let integer = 0;
+
+add.addEventListener('click', function(){
+  integer -=1;
+  int.innerHTML = integer;
+})
+
+add.addEventListener('click', function(){
+  integer +=1;
+  int.innerHTML = integer;
+})
